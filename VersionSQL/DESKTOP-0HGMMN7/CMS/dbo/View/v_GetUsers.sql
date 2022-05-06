3aefee4a6279820c6cb13747f1c4978cb7bb85ae
AdamBOD <adambarryodonovan@gmail.com>
2022-05-06 23:03:58 +0100

    Setup GetUsers view

---
+++ VersionSQL/DESKTOP-0HGMMN7/CMS/dbo/View/v_GetUsers.sql
@@ -0,0 +1,7 @@
+﻿/****** Object:  View [dbo].[v_GetUsers]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE VIEW dbo.v_GetUsers
+AS
+SELECT        U1.Id, U1.Email, U1.FirstName, U1.LastName, U1.IsAdmin, U1.IsVerified, U1.CreatedOn, U1.CreatedBy, U1.LastUpdatedOn, U1.LastUpdatedBy, U2.FirstName + ' ' + U2.LastName AS CreatedByName
+FROM            dbo.[User] AS U1 LEFT OUTER JOIN
+                         dbo.[User] AS U2 ON U2.Id = U1.CreatedBy      dbo.[User] AS U2 ON U2.Id = U1.CreatedBy
